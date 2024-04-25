68b6995fa46629b21d58b53ea9df0161aace8ece
StanislavPB <perminovsb@gmail.com>
2024-04-25 12:54:37 +0100

    init

---
+++ src/main/java/org/clientdemoapp/service/DeleteClientService.java
@@ -0,0 +1,33 @@
+package org.clientdemoapp.service;
+
+import org.clientdemoapp.entity.Client;
+import org.clientdemoapp.entity.RequestDto;
+import org.clientdemoapp.repository.ClientRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteClientService {
+
+    private final ClientRepository repository;
+
+    public DeleteClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
+
+    public ResponseEntity<Boolean> deleteClient(Integer id){
+
+        Boolean deleteResult = repository.delete(id);
+
+        if (deleteResult) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+
+
+}
